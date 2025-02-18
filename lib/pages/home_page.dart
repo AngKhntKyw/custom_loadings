15b9f0d1173418932174ee158b9b92a31c37046b
Aung Khant <aungkhantkyaw8918@gmail.com>
2025-02-18 17:01:28 +0630

    add pic

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
-import 'dart:developer';
+import 'dart:async';
 import 'package:custom_loadings/widgets/arc_reactor.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +17,11 @@ class _HomePageState extends State<HomePage>
   late AnimationController _controller;
   late Animation<double> _radiusAnimation;
   bool topLeft = false;
+  //
+  double _turns = 0.0;
+  late Timer _timer;
+  //
+  String _formattedTime = DateFormat('hh:mm a').format(DateTime.now());
 
   @override
   void initState() {
@@ -23,7 +29,7 @@ class _HomePageState extends State<HomePage>
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 1),
     );
 
     _radiusAnimation = Tween<double>(begin: 0, end: 20).animate(
@@ -32,8 +38,22 @@ class _HomePageState extends State<HomePage>
         curve: Curves.easeInOut,
       ),
     );
+    _startRotation();
+    _updateTime();
 
     _controller.forward();
+  }
+
+  void _updateTime() {
+    // Updates time every second
+    Future.delayed(const Duration(seconds: 1), () {
+      if (mounted) {
+        setState(() {
+          _formattedTime = DateFormat('hh:mm a').format(DateTime.now());
+        });
+        _updateTime();
+      }
+    });
   }
 
   void onTap() {
@@ -44,30 +64,84 @@ class _HomePageState extends State<HomePage>
     });
   }
 
+  void _startRotation() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _turns += 0.02;
+      });
+    });
+  }
+
   @override
   void dispose() {
     _controller.dispose();
+    _timer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    log(_radiusAnimation.value.toString());
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: GestureDetector(
         onTap: onTap,
         child: Stack(
           children: [
+            //
+            AnimatedPositioned(
+              top: topLeft ? size.height / 10 : size.height + 10,
+              right: 0,
+              left: 0,
+              duration: const Duration(seconds: 1),
+              child: Image.network(
+                "assets/wallpaperflare.com_wallpaper.jpg",
+                height: size.height / 1.5,
+                width: size.height / 1.5,
+              ),
+            ),
+
+            //
+            AnimatedPositioned(
+              right: 10,
+              bottom: !topLeft ? -1000 - (_radiusAnimation.value + 80) : 20,
+              duration: const Duration(seconds: 1),
+              child: CustomPaint(
+                size: const Size(150, 150),
+                painter: FolderIconPainter(),
+              ),
+            ),
+
+            //
+
+            AnimatedPositioned(
+              duration: const Duration(seconds: 1),
+              left: 10,
+              bottom: !topLeft ? -1000 - (_radiusAnimation.value + 80) : 20,
+              child: AnimatedRotation(
+                duration: const Duration(seconds: 1),
+                turns: _turns,
+                curve: Curves.bounceIn,
+                child: ClipOval(
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(
+                    "assets/pxfuel.jpg",
+                    fit: BoxFit.cover,
+                    width: size.height / 3,
+                    height: size.height / 3,
+                  ),
+                ),
+              ),
+            ),
+
+            //
             AnimatedPositioned(
               duration: const Duration(seconds: 1),
               left: !topLeft
-                  ? (MediaQuery.of(context).size.width / 2) -
-                      (_radiusAnimation.value + 80)
-                  : 40,
+                  ? (size.width / 2) - (_radiusAnimation.value + 80)
+                  : 10,
               top: !topLeft
-                  ? (MediaQuery.of(context).size.height / 2) -
-                      (_radiusAnimation.value + 80)
-                  : 40,
+                  ? (size.height / 2) - (_radiusAnimation.value + 80)
+                  : 20,
               child: AnimatedBuilder(
                 animation: _radiusAnimation,
                 builder: (context, child) {
@@ -77,9 +151,88 @@ class _HomePageState extends State<HomePage>
                 },
               ),
             ),
+
+            //
+
+            //
+            AnimatedPositioned(
+              top: 20,
+              right: topLeft ? 10 : -1000,
+              duration: const Duration(seconds: 1),
+              child: Row(
+                children: [
+                  SizedBox(width: size.width / 40),
+                  Text(
+                    _formattedTime,
+                    style: const TextStyle(
+                      fontFamily: 'Digital',
+                      fontSize: 40,
+                      color: Color.fromARGB(255, 112, 236, 255),
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          color: Color.fromARGB(255, 112, 236, 255),
+                          blurRadius: 0.1,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //
           ],
         ),
       ),
     );
+  }
+}
+
+class FolderIconPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = Colors.yellow
+      ..style = PaintingStyle.fill;
+
+    final Paint outlinePaint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+
+    // Folder shape (rounded rectangle)
+    final folderRect = Rect.fromLTWH(20, 30, size.width - 40, size.height - 60);
+    final folderPath = Path()
+      ..moveTo(folderRect.left + 10, folderRect.top) // Left side top
+      ..lineTo(folderRect.right - 10, folderRect.top) // Right side top
+      ..lineTo(folderRect.right - 10, folderRect.bottom) // Right side bottom
+      ..lineTo(folderRect.left + 10, folderRect.bottom) // Left side bottom
+      ..close();
+
+    // Draw the folder background
+    canvas.drawPath(folderPath, paint);
+
+    // Draw the folder outline
+    canvas.drawPath(folderPath, outlinePaint);
+
+    // Folder tab (Top part of the folder)
+    final tabPath = Path()
+      ..moveTo(folderRect.left, folderRect.top)
+      ..lineTo(folderRect.left + 40, folderRect.top)
+      ..lineTo(folderRect.left + 10, folderRect.top - 20)
+      ..close();
+
+    paint.color = Colors.orange; // Different color for tab
+    canvas.drawPath(tabPath, paint);
+
+    // Outline for the tab
+    outlinePaint.color = Colors.black;
+    canvas.drawPath(tabPath, outlinePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false; // No need to repaint, static icon
   }
 }
